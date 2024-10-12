870a7852b9e32aaa07f1e08614e996589486eb86
vikas kumar <vk.vikas3003@gmail.com>
2024-10-12 18:48:39 +0530

    add product page and its corresponding BE fetch logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import ProductCard from "@/components/ProductCard";
 import Searchbar from "@/components/Searchbar";
+import { getAllProducts } from "@/lib/actions";
 import Image from "next/image";
 
 const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -37,11 +41,11 @@ const Home = async () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
 
-        {/* <div className="flex flex-wrap gap-x-8 gap-y-16">
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
-        </div> */}
+        </div>
       </section>
     </>
   );
